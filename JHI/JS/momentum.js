8ebecd3578756b7a566bd27ce4068f7c2bd16969
Hyein Jung <penguin636@naver.com>
2023-06-20 17:42:27 +0900

    0620 과제 vanilla js

---
+++ JHI/JS/momentum.js
@@ -0,0 +1,55 @@
+//clock
+let clock = document.querySelector(".clock h1");
+
+function getClock() {
+  let date = new Date();
+  let hours = String(date.getHours()).padStart(2, "0");
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+  let seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
+//random background
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+
+bgImage.src = `../img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+
+//login
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+const greeting = document.querySelector(".greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreeting(savedUsername);
+}
