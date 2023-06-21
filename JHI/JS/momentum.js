364b847e9c09219c6a53f1927063ac1555954455
Hyein Jung <penguin636@naver.com>
2023-06-21 15:05:23 +0900

    수정용

--- JHI/JS/momentum.js
+++ JHI/JS/momentum.js
@@ -6,7 +6,7 @@ function getClock() {
   let hours = String(date.getHours()).padStart(2, "0");
   let minutes = String(date.getMinutes()).padStart(2, "0");
   let seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+  clock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
@@ -38,7 +38,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreeting(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Hello! ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
