747e7a12c4730c12584871e266823ed187aa39e8
Hyein Jung <penguin636@naver.com>
2023-06-22 17:47:37 +0900

    0622 과제 vanilla js

--- JHI/JS/momentum.js
+++ JHI/JS/momentum.js
@@ -13,11 +13,18 @@ getClock();
 setInterval(getClock, 1000);
 
 //random background
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = [
+  "https://images.unsplash.com/photo-1686854010079-455de3e8db41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1660799950423-15baac67ddf1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2832&q=80",
+  "https://images.unsplash.com/photo-1685901088371-f498db7f8c46?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2831&q=80",
+  "https://images.unsplash.com/photo-1535320404287-416e2c6d2b41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1538370965046-79c0d6907d47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80",
+  "https://images.unsplash.com/photo-1540270776932-e72e7c2d11cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
-bgImage.src = `../img/${chosenImage}`;
+bgImage.src = `${chosenImage}`;
 
 document.body.appendChild(bgImage);
 
@@ -52,4 +59,95 @@ if (savedUsername === null) {
   // show the greetings
   loginForm.classList.add(HIDDEN_CLASSNAME);
   paintGreeting(savedUsername);
+}
+
+//weather API
+const weather = document.getElementById("weather");
+
+const API_KEY = "6313accb82f2d39440fbe74e2778d554";
+
+function getWeather(lat, lon) {
+  //console.log(lat, lon);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json);
+      const temp = json.main.temp + "℃";
+      const humidity = json.main.humidity;
+      const city = json.name;
+      const weatherM = json.weather[0].main;
+      weather.innerHTML = `${city} ${weatherM} ${temp} ${humidity}%`;
+    });
+}
+
+function hadleGeoSuccess(pos) {
+  const latitude = pos.coords.latitude;
+  const longitude = pos.coords.longitude;
+  getWeather(latitude, longitude);
+}
+
+function hadleGeoError() {}
+console.log("error");
+function init() {
+  navigator.geolocation.getCurrentPosition(hadleGeoSuccess, hadleGeoError);
+}
+
+init();
+
+//todo
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+let toDos = [];
+
+function savetoDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentNode;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  savetoDos();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  savetoDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem("todos");
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
