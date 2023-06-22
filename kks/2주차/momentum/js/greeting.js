aebbd37642f19c4bea13c963dc624f21a3f89fa7
kangkyeongseo <kks_big@naver.com>
2023-06-22 18:41:26 +0900

    0622 과제 투두리스트

--- kks/2주차/momentum/js/greeting.js
+++ kks/2주차/momentum/js/greeting.js
@@ -1,22 +1,50 @@
 const greetingForm = document.querySelector(".greeting__form");
 const greetingTitle = document.querySelector(".greeting__title");
 const greetingInput = greetingForm.querySelector(".greeting__form__input");
+const changeBtn = document.querySelector(".change-name-btn");
+const todoSection = document.querySelector(".todo");
 
-const onGreeting = (event) => {
-  event.preventDefault();
+const NAME = "name";
+
+const getLocalName = () => {
+  const localName = localStorage.getItem(NAME);
+  if (localName) {
+    printeGreeting(localName);
+    greetingInput.value = localName;
+  }
+};
+
+const onChangeName = () => {
+  greetingForm.classList.remove("hidden");
+  changeBtn.classList.add("hidden--visi");
+  todoSection.classList.add("hidden");
+};
+
+const printeGreeting = (name) => {
   const time = new Date().getHours();
   let greetingMsg;
   if (time >= 6 && time < 12) {
-    greetingMsg = `Good morning, ${greetingInput.value}.`;
+    greetingMsg = `Good morning, ${name}.`;
   } else if (time >= 12 && time < 18) {
-    greetingMsg = `Good afternoom, ${greetingInput.value}.`;
+    greetingMsg = `Good afternoom, ${name}.`;
   } else if (time >= 18 && time < 22) {
-    greetingMsg = `Good evening, ${greetingInput.value}.`;
+    greetingMsg = `Good evening, ${name}.`;
   } else {
-    greetingMsg = `Good night, ${greetingInput.value}.`;
+    greetingMsg = `Good night, ${name}.`;
   }
   greetingTitle.innerText = greetingMsg;
-  greetingInput.classList.add("hidden");
+  greetingForm.classList.add("hidden");
+  changeBtn.classList.remove("hidden--visi");
+  todoSection.classList.remove("hidden");
+};
+
+const onGreeting = (event) => {
+  event.preventDefault();
+  printeGreeting(greetingInput.value);
+  localStorage.setItem(NAME, greetingInput.value);
 };
 
 greetingForm.addEventListener("submit", onGreeting);
+changeBtn.addEventListener("click", onChangeName);
+
+getLocalName();
